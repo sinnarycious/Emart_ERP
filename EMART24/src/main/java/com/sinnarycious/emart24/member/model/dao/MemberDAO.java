239e0885bcc0bb2836a181336183de39c53b1682
SUBIN <SUBIN@58.123.142.203>
2021-06-11 16:43:13 +0900

    0611 pm 4:00 subin

--- EMART24/src/main/java/com/sinnarycious/emart24/member/model/dao/MemberDAO.java
+++ EMART24/src/main/java/com/sinnarycious/emart24/member/model/dao/MemberDAO.java
@@ -5,22 +5,9 @@ import org.springframework.ui.Model;
 import com.sinnarycious.emart24.member.model.vo.Member;
 
 public interface MemberDAO {
-<<<<<<< HEAD
 	
-<<<<<<< HEAD
 	Member selectOneMember(Member m);
-=======
-
->>>>>>> refs/remotes/origin/kjw1302
 	
-=======
->>>>>>> refs/remotes/origin/gayul
 	int updateMember(Member member);
-
-<<<<<<< HEAD
-	
-=======
-	Member selectOneMember(String userId);
->>>>>>> refs/remotes/origin/gayul
 
 }
--- EMART24/src/main/java/com/sinnarycious/emart24/member/model/service/MemberService.java
+++ EMART24/src/main/java/com/sinnarycious/emart24/member/model/service/MemberService.java
@@ -6,21 +6,8 @@ import com.sinnarycious.emart24.member.model.vo.Member;
 
 public interface MemberService {
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
 	Member selectOneMember(Member m);
-=======
 	
->>>>>>> refs/remotes/origin/gayul
-	
-=======
->>>>>>> refs/remotes/origin/kjw1302
 	int updateMember(Member member);
-
-<<<<<<< HEAD
-=======
-	Member selectOneMember(String userId);
->>>>>>> refs/remotes/origin/gayul
-	
 
 }
